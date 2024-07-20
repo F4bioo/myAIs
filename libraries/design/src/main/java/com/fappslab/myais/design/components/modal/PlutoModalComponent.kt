7eeb0336c1a8741ef9a661228f629092c486949c
F4bioo <costa.fbo@gmail.com>
2024-07-20 00:58:41 -0300

    Changed: Setup new Gemini model

--- libraries/design/src/main/java/com/fappslab/myais/design/components/modal/PlutoModalComponent.kt
+++ libraries/design/src/main/java/com/fappslab/myais/design/components/modal/PlutoModalComponent.kt
@@ -2,6 +2,7 @@ package com.fappslab.myais.design.components.modal
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,18 +14,20 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalBottomSheetProperties
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.window.SecureFlagPolicy
 import com.fappslab.myais.design.R
 import com.fappslab.myais.design.accessibility.semantics
 import com.fappslab.myais.design.components.button.ButtonType
