f2fb8e7e8fcb85010a77273a43a91c10debf1033
F4bioo <costa.fbo@gmail.com>
2024-08-03 11:44:59 -0300

    Added: Create Dp extension

--- libraries/design/src/main/java/com/fappslab/myais/libraries/design/extension/ComposeExtension.kt
+++ libraries/design/src/main/java/com/fappslab/myais/libraries/design/extension/ComposeExtension.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.fappslab.myais.libraries.design.components.button.ClickType
 import com.fappslab.myais.libraries.design.components.button.rememberClickAction
 import com.fappslab.myais.libraries.design.theme.tokens.PlutoRadius
@@ -26,4 +29,16 @@ fun Modifier.clickable(
             role = role,
             onClick = rememberClickAction(clickType = clickType, action = onClicked)
         )
+}
+
+@Composable
+fun Float.toDp(): Dp {
+    val density = LocalDensity.current.density
+    return (this / density).dp
+}
+
+@Composable
+fun Dp.toPx(): Float {
+    val density = LocalDensity.current.density
+    return value * density
 }
